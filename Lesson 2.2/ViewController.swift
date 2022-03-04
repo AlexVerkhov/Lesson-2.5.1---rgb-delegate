7dbcf0234bd5bba9c779b5efbce8bd8f6a7f60eb
AlexVerkhov <alexverkhov@gmail.com>
2022-03-04 14:45:01 +0700

    Done

--- Lesson 2.2/ViewController.swift
+++ Lesson 2.2/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var palete: UIView!
+    
+    @IBOutlet var redCountLabel: UILabel!
+    @IBOutlet var greenCountLabel: UILabel!
+    @IBOutlet var blueCountLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        palete.layer.cornerRadius = 20
+        updateColor()
     }
 
-
+    @IBAction func redColorSliderUpdate() {
+        updateColor()
+    }
+    
+    @IBAction func greenColorSliderUpdate() {
+        updateColor()
+    }
+    
+    @IBAction func blueColorSliderUpdate() {
+        updateColor()
+    }
+    
+    func updateColor() {
+        let redColor = redSlider.value
+        let greenColor = greenSlider.value
+        let blueColor = blueSlider.value
+        
+        redCountLabel.text = String(round(redColor * 100) / 100)
+        greenCountLabel.text = String(round(greenColor * 100) / 100)
+        blueCountLabel.text = String(round(blueColor * 100) / 100)
+        
+        palete.backgroundColor = UIColor(
+            red: CGFloat(redColor),
+            green: CGFloat(greenColor),
+            blue: CGFloat(blueColor),
+            alpha: 1
+        )
+    }
 }
-
